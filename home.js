ee1b20e188c1789996abd2e3f86ab53da39f6f52
KevinsProgramming <B00157434@mytudublin.ie>
2024-12-03 20:20:54 +0000

    NC6

--- home.js
+++
@@ -1,9 +0,0 @@
-const navList = document.querySelector('ul');
-
-window.addEventListener('resize', function () {
-  if (window.innerWidth <= 600) {
-    navList.classList.add('show');
-  } else {
-    navList.classList.remove('show');
-  }
-});
