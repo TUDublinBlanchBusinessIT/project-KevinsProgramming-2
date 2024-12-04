dd9f7c17f2afd9c3fbe5c683662f70d45815e7be
KevinsProgramming <B00157434@mytudublin.ie>
2024-12-04 00:02:51 +0000

    NC12

---
+++ App.js
@@ -0,0 +1,4 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, StyleSheet, ScrollView, Linking, Alert, Dimensions, } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
